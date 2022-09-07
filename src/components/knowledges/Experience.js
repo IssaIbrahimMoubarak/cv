56ed8141106e2d28673fb97785c5b2ad860fc17d
ISSA IBRAHIM <issaibrahimmoubarak@gmail.com>
2022-09-07 12:56:28 +0100

    code source

---
+++ src/components/knowledges/Experience.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Experience = () => {
+	return (
+		<div className="experience">
+			<h3>Certifications et Attestations</h3>
+			<div className="exp-1">
+				<h4>Certification Chez Google :</h4>
+				<p>
+					Certification en Principes Fondamentaux du marketing numérique crée
+					par Google, sous la tutelle du Président de Google, zone EMEA M. Matt
+					Brittin et PDG de l’IAB Europe Mme Townsend Feehan Pamela. <br />
+					<strong>
+						https://learndigital.withgoogle.com/lateliernumerique/validate-certificate-code
+					</strong>
+					<br /> <strong>Id du Certificat</strong> : P2V AWN KDQ
+				</p>
+			</div>
+			<div className="exp-2">
+				<h4>Les attestations de FRANCE UNIVERSITE NUMERIQUE (FUN-MOOC) :</h4>
+				<p>
+					<li>
+						<ul>
+							<strong>En Data Science et à ses enjeux</strong> de l’université
+							PARIS CERGY (CY UNIVERSITE), obtenu le 20/12/2021.
+						</ul>
+						<ul>
+							<strong>En Langue Vivante (Anglais) : Unlock Your English</strong>{" "}
+							de l’universite IMT, obtenu le 17/06/2021.
+						</ul>
+						<ul>
+							<strong>
+								En Base de données relationnelles : apprendre pour utiliser
+							</strong>
+							de l’université le CNAM, obtenu le 09/03/2021.
+						</ul>
+					</li>
+				</p>
+			</div>
+			<div className="exp-3">
+				<h4>Ambassadeur JNJL3 depuis 29/05/2021 chez Leader Afrique</h4>
+			</div>
+		</div>
+	);
+};
+
+export default Experience;
